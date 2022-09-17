d9814b565e33ce6174cfc86ea81ce15e684269b5
ahmedalifzd <113782215+ahmedalifzd@users.noreply.github.com>
2022-09-17 18:07:31 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  createCanvas(400,400);
+
+box = createSprite(200,200,50,50)
+
+}
+
+
+function draw() 
+{
+  background(30);
+
+if (keyIsDown(RIGHT_ARROW)){
+box.position.x+=2
+}
+if (keyIsDown(LEFT_ARROW)){
+box.position.x-=2
+}
+if(keyIsDown(UP_ARROW)){
+ box.position.y-=2 
+}
+if (keyIsDown(DOWN_ARROW)){
+  box.position.y+=2
+}
+
+
+drawSprites()
+}
+
+
+
